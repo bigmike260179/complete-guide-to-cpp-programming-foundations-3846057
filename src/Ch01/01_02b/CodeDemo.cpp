6c376eddb90295920f1614a52dcbe6ba97e4cab2
bigmike260179 <michael_heino@hotmail.com>
2026-01-28 14:59:50 +0000

    Completed Hello world program

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,14 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include<iostream>
+
+int main()
+{
+    std::cout << "Hello World!!" << '\n';     // std::endl works like '\n'
+
+    std::cout << '\n' << '\n';
+
+    return 0;
+}

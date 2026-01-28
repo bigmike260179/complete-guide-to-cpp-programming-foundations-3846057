f259a834ada72e1a0643e38896c7d1cd39ad86be
bigmike260179 <michael_heino@hotmail.com>
2026-01-28 16:24:06 +0000

    Followed along during variables exercise

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,29 @@
 
 #include <iostream>
 
-int main(){
-    
+int a, b = 5;           // Global variables... to be avoided
+
+int main()
+{
+    bool myFlag;        // Local variable
+    a = 7;
+    myFlag = false;
+
+
+    std::cout << a << '\n';
+    std::cout << b << '\n';
+    std::cout << "Flag = " << myFlag << '\n';
+    std::cout << "Subtraction answer: " << (a - b) << '\n';
+    std::cout << "Addition answer: " << (a + b) << '\n';
+
+    myFlag = true;
+    std::cout << "Flag = " << myFlag << '\n';
+
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "Unsigned Subtraction answer: " << (b - a) << '\n';
+
     std::cout << std::endl << std::endl;
+
     return 0;
 }

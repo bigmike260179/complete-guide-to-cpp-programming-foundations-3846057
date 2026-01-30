c882d4bc7def1398f810d8c1d75e69d8a396a415
bigmike260179 <michael_heino@hotmail.com>
2026-01-30 16:15:35 +0000

    dpne

--- src/Ch04/04_02b/CodeDemo.cpp
+++ src/Ch04/04_02b/CodeDemo.cpp
@@ -8,14 +8,15 @@
 int main(){
     int a = 37;
     int *ptr;
+    
+    //ptr = nullptr;              // Does not point to any memory at the moment
+    ptr = &a;
 
-    ptr = nullptr;
-
-    std::cout << "             The value of a is " << ptr << std::endl;
+    std::cout << "             The value of a is " << a << std::endl;
     std::cout << "    ptr is pointing to address " << ptr << std::endl;
-    std::cout << "           The address of a is " << ptr << std::endl;
-    std::cout << "Where ptr is pointing, we have " << ptr << std::endl;
-    std::cout << "         The address of ptr is " << ptr << std::endl;
+    std::cout << "           The address of a is " << &a << std::endl;
+    std::cout << "Where ptr is pointing, we have " << *ptr << std::endl;
+    std::cout << "         The address of ptr is " << &ptr << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
